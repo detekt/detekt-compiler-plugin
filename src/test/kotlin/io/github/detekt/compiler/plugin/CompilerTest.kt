e3219b2e6d1a8aea68db33d66320f8476751390f
Matthew Haughton <3flex@users.noreply.github.com>
2020-08-24 02:12:19 +1000

    Smoke test with kotlin-compile-testing (#5)
    Co-authored-by: Artur Bosch <arturbosch@gmx.de>

---
+++ src/test/kotlin/io/github/detekt/compiler/plugin/CompilerTest.kt
@@ -0,0 +1,31 @@
+package io.github.detekt.compiler.plugin
+
+import com.tschuchort.compiletesting.KotlinCompilation
+import com.tschuchort.compiletesting.SourceFile
+import org.assertj.core.api.Assertions.assertThat
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object CompilerTest: Spek({
+    describe("smoke test") {
+        val kotlinSource = SourceFile.kotlin("KClass.kt", """
+        class KClass {
+            fun foo() {
+                val x = 3
+                println(x)
+            }
+        }
+    """)
+
+        val result = KotlinCompilation().apply {
+            sources = listOf(kotlinSource)
+
+            compilerPlugins = listOf(DetektComponentRegistrar())
+            commandLineProcessors = listOf(DetektCommandLineProcessor())
+        }.compile()
+
+        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+        assertThat(result.messages).contains("MagicNumber")
+        assertThat(result.messages).contains("Success?: false")
+    }
+})
