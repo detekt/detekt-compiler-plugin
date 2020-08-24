30b1aada945678f902fb5f4f087c7b6e34a6d75e
Matthew Haughton <3flex@users.noreply.github.com>
2020-08-24 16:25:04 +1000

    Add kotlinc integration test task "testPluginKotlinc"

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
+import de.undercouch.gradle.tasks.download.Download
+import de.undercouch.gradle.tasks.download.Verify
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.ByteArrayOutputStream
 
 val assertJVersion: String by project
 val detektVersion: String by project
 val kotlinVersion: String by project
+val kotlinCompilerChecksum: String by project
 val kotlinCompileTestVersion: String by project
 val detektPluginVersion: String by project
 val spekVersion: String by project
@@ -68,6 +72,51 @@ tasks.shadowJar {
         exclude(dependency("org.jetbrains.kotlin:kotlin-script-runtime"))
         exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib"))
         exclude(dependency("org.jetbrains.kotlin:kotlin-stdlib-common"))
+    }
+}
+
+val verifyKotlinCompilerDownload by tasks.creating(Verify::class) {
+    src(file("$rootDir/.kotlinc/kotlin-compiler-$kotlinVersion.zip"))
+    algorithm("SHA-256")
+    checksum(kotlinCompilerChecksum)
+    outputs.upToDateWhen { true }
+}
+
+val downloadKotlinCompiler by tasks.creating(Download::class) {
+    src("https://github.com/JetBrains/kotlin/releases/download/v$kotlinVersion/kotlin-compiler-$kotlinVersion.zip")
+    dest(file("$rootDir/.kotlinc/kotlin-compiler-$kotlinVersion.zip"))
+    overwrite(false)
+    finalizedBy(verifyKotlinCompilerDownload)
+}
+
+val unzipKotlinCompiler by tasks.creating(Copy::class) {
+    dependsOn(downloadKotlinCompiler)
+    from(zipTree(downloadKotlinCompiler.dest))
+    into(file("$rootDir/.kotlinc/$kotlinVersion"))
+}
+
+val testPluginKotlinc by tasks.creating(RunTestExecutable::class) {
+    dependsOn(unzipKotlinCompiler, tasks.shadowJar)
+    executable(file("${unzipKotlinCompiler.destinationDir}/kotlinc/bin/kotlinc"))
+    args(
+        listOf(
+            "$rootDir/src/test/resources/hello.kt",
+            "-Xplugin=${tasks.shadowJar.get().archiveFile.get().asFile.absolutePath}",
+            "-P",
+            "plugin:detekt-compiler-plugin:debug=true"
+        )
+    )
+    errorOutput = ByteArrayOutputStream()
+    // dummy path - required for RunTestExecutable task but doesn't do anything
+    outputDir = file("$buildDir/tmp/kotlinc")
+
+    doLast {
+        if (!errorOutput.toString().contains("MagicNumber - [x] at hello.kt")) {
+            throw GradleException(
+                "kotlinc $kotlinVersion run with compiler plugin did not find MagicNumber issue as expected"
+            )
+        }
+        (this as RunTestExecutable).execResult!!.assertNormalExitValue()
     }
 }
 
