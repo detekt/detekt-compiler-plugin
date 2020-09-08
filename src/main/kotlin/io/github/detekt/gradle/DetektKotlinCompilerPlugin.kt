bf7eca2335eab0f9a9ee310d083dd563b30f79c5
Matthew Haughton <3flex@users.noreply.github.com>
2020-09-09 01:07:05 +1000

    Update to Kotlin 1.4.0 (#7)
    * Update to Kotlin 1.4.0
    
    * Run smoke test in CI
    
    * Migrate to KotlinCompilerPluginSupportPlugin
    
    The old KotlinGradleSubplugin interface is deprecated from Kotlin 1.4

--- src/main/kotlin/io/github/detekt/gradle/DetektKotlinCompilerPlugin.kt
+++ src/main/kotlin/io/github/detekt/gradle/DetektKotlinCompilerPlugin.kt
@@ -3,33 +3,31 @@ package io.github.detekt.gradle
 import io.github.detekt.compiler.plugin.Options
 import io.github.detekt.gradle.extensions.DetektExtension
 import org.gradle.api.Project
-import org.gradle.api.tasks.compile.AbstractCompile
-import org.jetbrains.kotlin.gradle.dsl.KotlinCommonOptions
+import org.gradle.api.provider.Provider
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
-import org.jetbrains.kotlin.gradle.plugin.KotlinGradleSubplugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
-class DetektKotlinCompilerPlugin : KotlinGradleSubplugin<AbstractCompile> {
+class DetektKotlinCompilerPlugin : KotlinCompilerPluginSupportPlugin {
 
-    override fun apply(
-        project: Project,
-        kotlinCompile: AbstractCompile,
-        javaCompile: AbstractCompile?,
-        variantData: Any?,
-        androidProjectHandler: Any?,
-        kotlinCompilation: KotlinCompilation<KotlinCommonOptions>?
-    ): List<SubpluginOption> {
+    private lateinit var project: Project
+
+    override fun apply(target: Project) {
+        project = target
+    }
+
+    override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
         val extension = project.extensions
             .findByType(DetektExtension::class.java)
             ?: DetektExtension(project)
 
-        val options = mutableListOf(
-            SubpluginOption(Options.debug, extension.debug.toString()),
-            SubpluginOption(Options.config, extension.config.joinToString(",")),
-            SubpluginOption(Options.isEnabled, extension.isEnabled.toString()),
-            SubpluginOption(Options.useDefaultConfig, extension.buildUponDefaultConfig.toString())
-        )
+        val options = project.objects.listProperty(SubpluginOption::class.java).apply {
+            add(SubpluginOption(Options.debug, extension.debug.toString()))
+            add(SubpluginOption(Options.config, extension.config.joinToString(",")))
+            add(SubpluginOption(Options.isEnabled, extension.isEnabled.toString()))
+            add(SubpluginOption(Options.useDefaultConfig, extension.buildUponDefaultConfig.toString()))
+        }
 
         extension.baseline?.let { options.add(SubpluginOption(Options.baseline, it.toString())) }
 
@@ -41,5 +39,5 @@ class DetektKotlinCompilerPlugin : KotlinGradleSubplugin<AbstractCompile> {
     override fun getPluginArtifact(): SubpluginArtifact =
         SubpluginArtifact("io.github.detekt", "detekt-compiler-plugin", "0.2.0") // TODO: generate version
 
-    override fun isApplicable(project: Project, task: AbstractCompile): Boolean = true
+    override fun isApplicable(kotlinCompilation: KotlinCompilation<*>): Boolean = true
 }
