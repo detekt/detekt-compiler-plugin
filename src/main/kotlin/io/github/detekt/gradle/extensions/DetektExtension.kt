bb569b6dfd00b61b3ecd8c4434ee75918c141f3b
Artur Bosch <arturbosch@gmx.de>
2020-09-10 09:50:50 +0200

    Inline extension defaults

--- src/main/kotlin/io/github/detekt/gradle/extensions/DetektExtension.kt
+++ src/main/kotlin/io/github/detekt/gradle/extensions/DetektExtension.kt
@@ -9,6 +9,15 @@ import java.io.File
 open class DetektExtension(project: Project) : CodeQualityExtension() {
 
     var isEnabled: Boolean = true
+    var baseline: File? = null
+    var debug: Boolean = false
+    var parallel: Boolean = false
+    var failFast: Boolean = false
+    var buildUponDefaultConfig: Boolean = true
+    var disableDefaultRuleSets: Boolean = false
+    var autoCorrect: Boolean = false
+
+    var config: ConfigurableFileCollection = project.objects.fileCollection()
 
     var ignoreFailures: Boolean
         @JvmName("ignoreFailures_")
@@ -18,38 +27,15 @@ open class DetektExtension(project: Project) : CodeQualityExtension() {
             isIgnoreFailures = value
         }
 
+    var input: ConfigurableFileCollection =
+        project.objects.fileCollection().from(DEFAULT_SRC_DIR_JAVA, DEFAULT_SRC_DIR_KOTLIN)
+
     val reports = DetektReports()
 
     fun reports(configure: Action<DetektReports>) = configure.execute(reports)
 
-    var input: ConfigurableFileCollection =
-        project.objects.fileCollection().from(DEFAULT_SRC_DIR_JAVA, DEFAULT_SRC_DIR_KOTLIN)
-
-    var baseline: File? = null
-
-    var config: ConfigurableFileCollection = project.objects.fileCollection()
-
-    var debug: Boolean = DEFAULT_DEBUG_VALUE
-
-    var parallel: Boolean = DEFAULT_PARALLEL_VALUE
-
-    var failFast: Boolean = DEFAULT_FAIL_FAST_VALUE
-
-    var buildUponDefaultConfig: Boolean = DEFAULT_BUILD_UPON_DEFAULT_CONFIG_VALUE
-
-    var disableDefaultRuleSets: Boolean = DEFAULT_DISABLE_RULESETS_VALUE
-
-    var autoCorrect: Boolean = DEFAULT_AUTO_CORRECT_VALUE
-
     companion object {
         const val DEFAULT_SRC_DIR_JAVA = "src/main/java"
         const val DEFAULT_SRC_DIR_KOTLIN = "src/main/kotlin"
-        const val DEFAULT_DEBUG_VALUE = false
-        const val DEFAULT_PARALLEL_VALUE = false
-        const val DEFAULT_AUTO_CORRECT_VALUE = false
-        const val DEFAULT_DISABLE_RULESETS_VALUE = false
-        const val DEFAULT_REPORT_ENABLED_VALUE = true
-        const val DEFAULT_FAIL_FAST_VALUE = false
-        const val DEFAULT_BUILD_UPON_DEFAULT_CONFIG_VALUE = true
     }
 }
