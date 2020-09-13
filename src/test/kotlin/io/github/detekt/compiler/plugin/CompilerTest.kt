099d65e51056e3d0d3996e0fbd6d234bb80d8509
Artur Bosch <arturbosch@gmx.de>
2020-09-13 21:24:30 +0200

    Refactor smoke test to not be executed during discovery time

--- src/test/kotlin/io/github/detekt/compiler/plugin/CompilerTest.kt
+++ src/test/kotlin/io/github/detekt/compiler/plugin/CompilerTest.kt
@@ -6,26 +6,31 @@ import org.assertj.core.api.Assertions.assertThat
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
-object CompilerTest: Spek({
+object CompilerTest : Spek({
+
     describe("smoke test") {
-        val kotlinSource = SourceFile.kotlin("KClass.kt", """
-        class KClass {
-            fun foo() {
-                val x = 3
-                println(x)
-            }
+
+        val kotlinSource by memoized {
+            SourceFile.kotlin("KClass.kt", """
+                class KClass {
+                    fun foo() {
+                        val x = 3
+                        println(x)
+                    }
+                }
+            """)
         }
-    """)
 
-        val result = KotlinCompilation().apply {
-            sources = listOf(kotlinSource)
+        it("will successfully run detekt and find one issue") {
+            val result = KotlinCompilation().apply {
+                sources = listOf(kotlinSource)
+                compilerPlugins = listOf(DetektComponentRegistrar())
+                commandLineProcessors = listOf(DetektCommandLineProcessor())
+            }.compile()
 
-            compilerPlugins = listOf(DetektComponentRegistrar())
-            commandLineProcessors = listOf(DetektCommandLineProcessor())
-        }.compile()
-
-        assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
-        assertThat(result.messages).contains("MagicNumber")
-        assertThat(result.messages).contains("Success?: false")
+            assertThat(result.exitCode).isEqualTo(KotlinCompilation.ExitCode.OK)
+            assertThat(result.messages).contains("MagicNumber")
+            assertThat(result.messages).contains("Success?: false")
+        }
     }
 })
