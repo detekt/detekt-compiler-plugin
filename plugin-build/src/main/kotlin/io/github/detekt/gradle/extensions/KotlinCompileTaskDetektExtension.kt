c440969e218bd6bc600c6130bcccd6ebc1add1ba
Matthew Haughton <3flex@users.noreply.github.com>
2021-08-15 22:04:52 +1000

    Don't allow reassignment of Gradle properties (#48)

--- plugin-build/src/main/kotlin/io/github/detekt/gradle/extensions/KotlinCompileTaskDetektExtension.kt
+++ plugin-build/src/main/kotlin/io/github/detekt/gradle/extensions/KotlinCompileTaskDetektExtension.kt
@@ -27,9 +27,9 @@ open class KotlinCompileTaskDetektExtension(project: Project) {
     val isEnabled: Property<Boolean> = objects.property(Boolean::class.java)
     val debug: Property<Boolean> = objects.property(Boolean::class.java)
     val buildUponDefaultConfig: Property<Boolean> = objects.property(Boolean::class.java)
-    var allRules: Property<Boolean> = objects.property(Boolean::class.java)
-    var disableDefaultRuleSets: Property<Boolean> = objects.property(Boolean::class.java)
-    var parallel: Property<Boolean> = objects.property(Boolean::class.java)
+    val allRules: Property<Boolean> = objects.property(Boolean::class.java)
+    val disableDefaultRuleSets: Property<Boolean> = objects.property(Boolean::class.java)
+    val parallel: Property<Boolean> = objects.property(Boolean::class.java)
 
     val baseline: RegularFileProperty = objects.fileProperty()
     val config: ConfigurableFileCollection = objects.fileCollection()
