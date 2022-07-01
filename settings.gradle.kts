c0bc75b69d1f60308e2460cd505a185ab612021e
Chao Zhang <chao.zhang@instacart.com>
2022-07-01 13:47:18 -0700

    Integrate with Gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+import com.gradle.enterprise.gradleplugin.internal.extension.BuildScanExtensionWithHiddenFeatures
+
 rootProject.name = "detekt-compiler-plugin-composite-build"
 
 enableFeaturePreview("VERSION_CATALOGS")
@@ -11,3 +13,29 @@ dependencyResolutionManagement {
 }
 
 includeBuild("plugin-build")
+
+plugins {
+    `gradle-enterprise`
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "1.7.2"
+}
+
+val isCiBuild = System.getenv("CI") != null
+
+gradleEnterprise {
+    buildScan {
+        publishAlways()
+
+        // Publish to scans.gradle.com when `--scan` is used explicitly
+        if (!gradle.startParameter.isBuildScan) {
+            server = "https://ge.detekt.dev"
+            this as BuildScanExtensionWithHiddenFeatures
+            publishIfAuthenticated()
+        }
+
+        isUploadInBackground = !isCiBuild
+
+        capture {
+            isTaskInputFiles = true
+        }
+    }
+}
