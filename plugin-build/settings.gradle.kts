6751184ed7ae081fc0d3ec6509da7f89fb3b0655
Matthew Haughton <3flex@users.noreply.github.com>
2022-10-08 13:14:55 +1100

    Move versions catalog to root project

--- plugin-build/settings.gradle.kts
+++ plugin-build/settings.gradle.kts
@@ -7,3 +7,11 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,14 +4,6 @@ rootProject.name = "detekt-compiler-plugin-composite-build"
 
 enableFeaturePreview("VERSION_CATALOGS")
 
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("plugin-build/gradle/libs.versions.toml"))
-        }
-    }
-}
-
 includeBuild("plugin-build")
 
 plugins {
